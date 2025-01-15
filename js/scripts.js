9b37c3cb732d1fb64ae96c0c8687929ddfeead17
Rena S <sekiguchi.rena@gmail.com>
2025-01-14 21:01:46 -0500

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,39 @@
+let myName = 'Rena';
+document.write(myName);
+
+//Heyooooooo this is a single light comment
+/*This
+    is
+    a
+    multi-line
+    comment
+    */
+
+
+/*let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+document.write(doubleSize);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
+*/
+
+alert('Hello World!!!');
+
+
+
+
+let favoriteFood ='Ramen';
+document.write(favoriteFood);
+
+
+let escapeText = "I'm ready to go!";
+document.write(escapeText);
+
+let name = 'Rena';
+document.write('My name is ' + name + ' and I\'m ready to go!');
