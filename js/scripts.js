ead7c2ae03d870c3564ee78944587a14c6a36223
Rena S <sekiguchi.rena@gmail.com>
2025-01-28 15:30:32 -0500

    Added srray variable for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,5 @@
-let myName = 'Rena';
-document.write(myName);
-
-//Heyooooooo this is a single light comment
-/*This
-    is
-    a
-    multi-line
-    comment
-    */
-
-
-/*let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-*/
-
-alert('Hello World!!!');
-
-
-
-
-let favoriteFood ='Ramen';
-document.write(favoriteFood);
-
-
-let escapeText = "I'm ready to go!";
-document.write(escapeText);
-
-let name = 'Rena';
-document.write('My name is ' + name + ' and I\'m ready to go!');
+let pokemonList = [
+    {name: "pikachu", height: 0.4, type: "electric"},
+    {name: "snorlax", height: 2.1, type: "normal"},
+    {name: "jigglypuff", height: 0.7, type: ["fairy", "normal"]}
+];
