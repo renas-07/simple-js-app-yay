c3f4a4d7bad6503fd6eedeaa8ed4760f0cb3ddc0
Rena S <sekiguchi.rena@gmail.com>
2025-01-28 15:24:02 -0500

    Added pokemonList variable and assigned to array

--- js/scripts.js
+++ js/scripts.js
@@ -13,27 +13,3 @@ document.write(myName);
 /*let simpleAddition = 2 + 2;
 document.write(simpleAddition);
 
-let size = 100;
-let doubleSize = size * 2;
-document.write(doubleSize);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
-*/
-
-alert('Hello World!!!');
-
-
-
-
-let favoriteFood ='Ramen';
-document.write(favoriteFood);
-
-
-let escapeText = "I'm ready to go!";
-document.write(escapeText);
-
-let name = 'Rena';
-document.write('My name is ' + name + ' and I\'m ready to go!');
