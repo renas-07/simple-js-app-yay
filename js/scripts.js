2507f9bb49b13f5533aa312dacadb6f2c8a2ed98
Rena S <sekiguchi.rena@gmail.com>
2025-02-19 22:22:42 -0500

    Wrapped pokemonList in IIFE

--- js/scripts.js
+++ js/scripts.js
@@ -1,51 +1,67 @@
-let pokemonList = [
-    {
-        name: " Pikachu ",
-        height: 0.4,
-        type: "electric"
-    },
-    {
-        name: " Snorlax ",
-        height: 2.1,
-        type: "normal"
-    },
-    {
-        name: " Jigglypuff ",
-        height: 0.7,
-        type: ["fairy", "normal"]
-    }
-];
+// let pokemonList = [
+//   {
+//     name: " Pikachu ",
+//     height: 0.4,
+//     type: "electric"
+//   },
+//   {
+//     name: " Snorlax ",
+//     height: 2.1,
+//     type: "normal"
+//   },
+//   {
+//     name: " Jigglypuff ",
+//     height: 0.7,
+//     type: ["fairy", "normal"]
+//   }
+// ];
 
-pokemonList.forEach(function(pokemon){
+
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {
+      name: " Pikachu ",
+      height: 0.4,
+      type: "electric"
+    },
+    {
+      name: " Snorlax ",
+      height: 2.1,
+      type: "normal"
+    },
+    {
+      name: " Jigglypuff ",
+      height: 0.7,
+      type: ["fairy", "normal"]
+    }
+  ]; // empty array
+
+  function getAll() {
+    return pokemonList;
+  }
+  function add(item) {
+    pokemonList.push(item);
+  }
+
+  return {
+    getAll: getAll,
+    add: add
+  }
+})()
+
+
+
+console.log(pokemonRepository.getAll());
+console.log(pokemonRepository.add)
+
+
+
+
+// pokemonList.forEach(function(add) {
+//   document.write(pokemon.name + ' is ' + pokemon.height + ' m tall!');
+// });
+
+
+pokemonRepository.getAll().forEach(function(pokemon){
   document.write(pokemon.name + ' is ' + pokemon.height + ' m tall!');
 });
-
-// let pokemonRepository = (function () {
-//     let pokemonList = []; // empty array
-  
-//     return {
-//       add: function(pokemon) {
-//         pokemonList.push(pokemon);
-//       },
-//       getAll: function() {
-//         return pokemonList;
-//       }
-//     };
-//   })();
-  
-//   console.log(pokemonRepository.getAll()); // []
-//   pokemonRepository.add({ name: 'Pikachu' });
-//   console.log(pokemonRepository.getAll()); // [ { name: 'Pikachu' } ]
-
-
-// for (let i = 0; i < pokemonList.length; i++) {
-
-//     if (pokemonList[i].height > 1) {
-//         document.write (pokemonList[i].name + " (Height: " + pokemonList[i].height + " -Wow, that is a big Pokemon!" + ")" )
-//         } else (
-//         document.write(pokemonList[i].name +
-//             " (Height: " + pokemonList[i].height + ")"
-        
-//         )
-
-
